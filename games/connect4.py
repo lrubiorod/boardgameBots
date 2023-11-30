80695159ba8a8f199e2fcbfd4dcc161f06aeadce
Luis Rubio <l.rubiorod@gmail.com>
2023-11-30 17:55:25 +0100

    feat: include connect4 game and improve main interface

---
+++ games/connect4.py
@@ -0,0 +1,63 @@
+from game import Game
+
+
+class ConnectFour(Game):
+    def __init__(self):
+        self.board = [[' ' for _ in range(7)] for _ in range(6)]
+        self.current_winner = None
+
+    def print_board(self):
+        print(' 1 2 3 4 5 6 7')
+        for row in self.board:
+            print('|' + '|'.join(row) + '|')
+        print()
+        print(f'Current winner: {self.current_winner}')
+
+    def make_move(self, move, player):
+        column = move
+        if self.board[0][column] != ' ':
+            return False
+
+        row = next(row for row in reversed(self.board) if row[column] == ' ')
+        row[column] = 'X' if player == 1 else 'O'
+        if self.check_winner(column, row, player):
+            self.current_winner = player
+        return True
+
+    def undo_move(self, move):
+        column = move
+        for row in self.board:
+            if row[column] != ' ':
+                row[column] = ' '
+                break
+        self.current_winner = None
+
+    def check_winner(self, column, row, player):
+        letter = 'X' if player == 1 else 'O'
+        # Check horizontal, vertical, and both diagonals
+        row_index = self.board.index(row)
+        directions = [(0, 1), (1, 0), (1, 1), (1, -1)]
+        for dr, dc in directions:
+            count = 0
+            for i in range(-3, 4):
+                r, c = row_index + dr * i, column + dc * i
+                if 0 <= r < 6 and 0 <= c < 7 and self.board[r][c] == letter:
+                    count += 1
+                    if count == 4:
+                        return True
+                else:
+                    count = 0
+        return False
+
+    def get_available_moves(self):
+        available_moves = [i for i in range(7) if self.board[0][i] == ' ']
+        return [i for i in range(7) if self.board[0][i] == ' ']
+
+    def is_game_over(self):
+        return self.current_winner is not None or all(self.board[0][i] != ' ' for i in range(7))
+
+    def evaluate_game_state(self, player):
+        if self.current_winner:
+            return 1 if (self.current_winner == player) else -1
+        else:
+            return 0
--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 from games.tictactoe import TicTacToe
+from games.connect4 import ConnectFour
 from strategies.minimax import MinimaxPlayer
 
 
 def main():
-    game = TicTacToe()
-
-    player1 = 'human'
-    player2 = MinimaxPlayer(depth_limit=9, player=2)
+    game = choose_game()
+    player1, player2 = setup_players()
 
     turn = 1
 
@@ -44,6 +43,45 @@ def get_player_move(game, turn):
             print("Invalid movement. Try again!")
         except ValueError:
             print("Write a valid number please!")
+
+
+def choose_game():
+    while True:
+        choice = int(input("Which game would you like to play?\n 1: TicTacToe\n 2: Connect4\n"))
+        if choice == 1:
+            return TicTacToe()
+        elif choice == 2:
+            return ConnectFour()
+        else:
+            print("Invalid choice. Please try again.")
+
+
+def choose_player_type(player_number):
+    while True:
+        choice = int(input(f"Choose the type of player {player_number}:\n 1: Human\n 2: Minimax\n"))
+        if choice == 1:
+            return 'human'
+        elif choice == 2:
+            return 'minimax'
+        else:
+            print("Invalid choice. Please try again.")
+
+
+def setup_players():
+    player1 = 'human'
+    player2 = 'human'
+    player1_type = choose_player_type(1)
+    player2_type = choose_player_type(2)
+
+    if player1_type != 'human':
+        if player1_type == 'minimax':
+            player1 = MinimaxPlayer(depth_limit=7, player=1)
+
+    if player2_type != 'human':
+        if player2_type == 'minimax':
+            player2 = MinimaxPlayer(depth_limit=7, player=2)
+
+    return player1, player2
 
 
 if __name__ == "__main__":
